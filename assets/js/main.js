45e55aba34fc787428d0a2b509172a72e1439a28
dpknight <dpk@derekknight.ca>
2016-09-02 16:12:31 -0400

    Ninth Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
 // Doc Ready - jQuery Shorthand
-// $(function(){});
+$(function() {
 
-// Watch for window scroll
-$(window).scroll( function() {
-  workThumbScroll();
+  // Watch for window scroll
+  $(window).scroll( function() {
+    workThumbScroll();
+  });
+
+  // The Work section thumbnail scroll
+  function workThumbScroll() {
+    // Capture the position from the top
+    // How far have we scrolled
+    var winScroll = $(window).scrollTop();
+
+    console.log(winScroll);
+
+    // Target the thumbnail strip
+    $('.thumbnail-strip').css('background-position', 'center -' + winScroll + 'px');
+  }
+
 });
-
-// The Work section thumbnail scroll
-function workThumbScroll() {
-  // Capture the position from the top
-  // How far have we scrolled
-  var winScroll = $(window).scrollTop();
-
-  // Target the thumbnail strip
-  $('.thumb-strip').css('backgrond-position', 'center ' + winScroll + 'px')
-}
