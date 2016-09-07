8d385cc544bb59eada6d96c40b3e4347f5d6d84a
dpknight <dpk@derekknight.ca>
2016-09-07 01:19:54 -0400

    Twelfth Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,40 @@ $(function() {
     workThumbScroll();
   });
 
-  // The Work section thumbnail scroll
-  function workThumbScroll() {
-    // Capture the position from the top
-    // How far have we scrolled
-    var winScroll = $(window).scrollTop();
-
-    console.log(winScroll);
-
-    // Target the thumbnail strip
-    $('.thumbnail-strip').css('background-position', 'center -' + winScroll + 'px');
-  }
+  //
+  captionClick();
 
 });
+
+// The Work section thumbnail scroll
+function workThumbScroll() {
+  // Capture the position from the top
+  // How far have we scrolled
+  var winScroll = $(window).scrollTop();
+
+  //console.log(winScroll);
+
+  // Target the thumbnail strip
+  $('.thumbnail-strip').css('background-position', 'center -' + winScroll + 'px');
+}
+
+
+// Controls the Testimonial Captions
+function captionClick() {
+  $('.profile-pic').on('click', function() {
+    var $this = $(this)
+    var picTop = $this.position().top;
+    var vertMath = -1 * ( Math.round( picTop - 230 ) );
+
+    // console.log(Math.round(vertMath));
+    $this.parent().css('top', + vertMath + 'px');
+
+    $this.addClass('caption-open').siblings().removeClass('caption-open');
+  });
+
+  // When a profile-pic is clicked
+  // get the distance of the profile-pic from its parent
+  // move the whole container up 115px + the count
+  // add the caption-open class to the profile-pic, and animate the caption
+
+}
